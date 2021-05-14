af2ac05ff4e93fa310e2c02554684d8f75a7b421
Jake Studer <jacob.studer.dev@gmail.com>
2021-05-13 23:04:15 -0600

    updates

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,11 +6,31 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
 // Categories have many Products
+
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongstoMany(ProductTag, {
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE',
+});
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongstoMany(ProductTag, {
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE',
+});
 
 module.exports = {
   Product,
